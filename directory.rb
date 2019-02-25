7d9b45a393f480a04205f7c6b7cde5bcdf32210d
fo0s <fo0sies@gmail.com>
2019-02-25 12:58:26 +0000

    Refactored. Added input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,48 @@
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# # let's put all students into an array
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
+#
+# def print_header
+#   puts "The students of Villains Academy"
+#   puts "-------------"
+# end
+# def print(students)
+#   students.each do |student|
+#     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+#   end
+# end
+# def print_footer(names)
+#   puts "Overall, we have #{names.count} great students"
+# end
+# #nothing happens until we call the methods
+# print_header
+# print(students)
+# print_footer(students)
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -25,7 +55,8 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-#nothing happens until we call the methods
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
